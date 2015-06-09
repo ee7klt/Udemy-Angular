60c8666255f18f45a440548bb7661a24b541047b
ee7klt <kltah1@gmail.com>
2015-06-09 15:48:53 +0800

    custom directives

--- app.js
+++ app.js
@@ -3,14 +3,6 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
 
-myApp.service('nameService', function() {
-
-  var self = this;
-  this.name = 'John Doe';
-  this.namelength = function() {
-    return self.name.length;
-  };
-});
 
 myApp.config(function ($routeProvider) {
   $routeProvider
@@ -33,35 +25,30 @@ myApp.config(function ($routeProvider) {
 
 
 
-myApp.controller('mainController', ['$scope', '$log', 'nameService', function($scope,$log,nameService) {
+myApp.controller('mainController', ['$scope', '$log', function($scope,$log,nameService) {
 
-  $scope.name = nameService.name;
-$log.log(nameService.namelength());
-$log.log(nameService.name);
-$scope.$watch('name', function() {
-  nameService.name = $scope.name;
+
+
+
+
+
+
+}]);
+
+
+myApp.controller('secondController', ['$scope', '$log', '$routeParams',function($scope,$log,$routeParams,nameService) {
+
+
+
+
+
+
+}]);
+
+myApp.directive("searchResult", function() {
+  return {
+    restrict: 'CM',
+    templateUrl:  'directives/searchresult.html',
+    replace: true
+  };
 });
-
-
-
-
-
-}]);
-
-
-myApp.controller('secondController', ['$scope', '$log', '$routeParams', 'nameService', function($scope,$log,$routeParams,nameService) {
-
-  $scope.name = nameService.name;
-  $scope.num = $routeParams.num || 0;
-  $scope.$watch('name', function() {
-    nameService.name = $scope.name;
-  });
-
-  //$log.log($log);
-//  $log.log($scope);
-
-
-
-
-
-}]);
