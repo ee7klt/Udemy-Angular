ebbcdaf7734a44870a5564afe91881996f4b16e9
ee7klt <kltah1@gmail.com>
2015-06-13 17:21:39 +0800

    weatherApp: routes and controllers home, forecast

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 
 // MODULE
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+
+
+
+// ROUTES
+weatherApp.config (function ($routeProvider) {
+  $routeProvider
+
+  .when('/', {
+    templateUrl: 'pages/home.html',
+    controller: 'homeController'
+  })
+
+  .when('/forecast', {
+    templateUrl: 'pages/forecast.html',
+    controller: 'forecastController'
+  })
+
+
+});
+
+
+
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', '$log', function ($scope, $log) {
+
+
+}]);
+
+
+weatherApp.controller('forecastController', ['$scope', '$log', function ($scope, $log) {
+
+
+}]);
